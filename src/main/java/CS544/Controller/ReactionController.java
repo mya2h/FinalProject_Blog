03b727a048d87c7d4824c6185642362579d3d8e6
yonanland <yonininu@gmail.com>
2023-06-17 09:50:55 -0500

    Update on reaction

--- src/main/java/CS544/Controller/ReactionController.java
+++ src/main/java/CS544/Controller/ReactionController.java
@@ -3,29 +3,48 @@ package CS544.Controller;
 import CS544.Dao.IReactionDao;
 import CS544.Model.Reaction;
 import CS544.Service.ReactionService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/reaction")
+@RequestMapping( "/reaction")
 public class ReactionController {
 
-    @Autowired
+    @Autowired  // @Resource
     private ReactionService reactionService;
 
-    @PostMapping("/add")             // add reaction
-    public String addReaction(@RequestBody Reaction reaction){
-        return "addReaction";
+    @GetMapping("/")
+    public List<Reaction> getAll(){
+        return reactionService.getAllReactions();
     }
 
-    @GetMapping("/delete")
-    public String deleteReaction(){
-        return "deleteReaction";
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Reaction get(@PathVariable long id){
+        return reactionService.getReaction(id);
     }
 
-    @GetMapping("/update")
-    public String updateReaction(){
-        return "updateReaction";
+    @PostMapping(value = "/add")             // add reaction
+    public RedirectView addReaction(@Valid @RequestBody Reaction reaction){
+        reactionService.saveReaction(reaction);
+        return new RedirectView("/reaction/" + reaction.getId());
+    }
+
+    @PutMapping(value = "/add/{id}")
+    public void put(@PathVariable long id, @Valid @RequestBody Reaction reaction){
+        if (id != reaction.getId()) {
+            throw new IllegalArgumentException();
+        }
+        reactionService.updateReaction(reaction);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public void delete(@PathVariable long id){
+        reactionService.deleteReaction(id);
     }
 }
